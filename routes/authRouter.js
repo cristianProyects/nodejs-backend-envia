6683669cae79461ed7f986bb61ea32deed8519a9
Cristian <crisespiridion405@gmail.com>
2024-02-20 23:54:32 -0600

    modify[configuration token] and add[enpoint]

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,30 +1,42 @@
-const express = require('express');
-const passport = require('passport');
-const jsonWebToken = require('jsonwebtoken');
-const { config } = require('../config/config');
+const express = require("express");
+const passport = require("passport");
+const jsonWebToken = require("jsonwebtoken");
+const { config } = require("../config/config");
 
 const router = express.Router();
 
-router.post('/login',
-    passport.authenticate('local', {session: false}),
-    async (req, res, next) => {
-        try {
-            const user = req.user;
-            const payload = {
-            sub: user.id,
-            role: user.role,
-            // expiresIn: 1000
-            }
-            const token =  jsonWebToken.sign(payload, config.jwtSecret);
-            res.json({
-            user,
-            token
-            });
-        } catch (error) {
-            next(error);
-            
-        }
+router.get(
+  "/verify/:token",
+  async (req, res, next) => {
+    try {
+        const { token } = req.params;
+        jsonWebToken.verify(token, config.jwtSecret);
+        return res.status(200).json({ message: 'Autorizado'});
+      } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized', });
+      }
+  },
+);
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  async (req, res, next) => {
+    try {
+      const user = req.user;
+      const payload = {
+        sub: user.id,
+        role: user.role,
+        // expiresIn: 1000
+      };
+      const token = jsonWebToken.sign(payload, config.jwtSecret);
+      res.json({
+        user,
+        token,
+      });
+    } catch (error) {
+      next(error);
     }
+  },
 );
 
 module.exports = router;
--- utils/sockets/index.js
+++ utils/sockets/index.js
@@ -1,13 +1,20 @@
+const shippingService = require("../../services/shippingServices");
+const shipping = new shippingService();
+
 module.exports = async (io) => {
   console.log("Sockets ON");
   io.on("connection", (socket) => {
     socket.on("inicio", async (data) => {
-      console.log(`CONNECT-USER-${data}`);
-      socket.join(`CONNECT-USER-${data}`);
+      socket.join(`CONNECT-USER-${data}`); // JOIN ROOM
     });
 
     socket.on("generateQuote", async (data) => {
-        console.log(data);
-      });
+        // Get data for  generateQuote
+      const response = await shipping.generateQuoteShipment(data); // Generate quote
+     
+      if (response){ 
+        socket.emit("count", 1); // For response add one more for count
+    }
+    });
   });
 };
